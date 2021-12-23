0633f49fa56c27db7761044c439240e8c1210538
Thodsapon Tantitham <55072426+benzthodsapon@users.noreply.github.com>
2021-12-23 14:25:30 +0700

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,29 @@
+import { render } from "@testing-library/react";
+import React,{ Component } from "react";
+import NavItems from './NavItems'
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'NavItemActive' : ''
+        }
+    }
+    render(){
+        return (
+             <nav>
+            <ul>
+                <NavItems item = "Home" tolink ="/"></NavItems>
+                <NavItems item = "About" tolink = "/about"></NavItems>
+                <NavItems item = "Education" tolink ="/education"></NavItems>
+                <NavItems item = "Skills" tolink ="/skills"></NavItems>
+                <NavItems item = "Contact" tolink ="/contact"></NavItems>
+            </ul>
+        </nav>
+        )
+       
+    }
+    
+}
+
+export default Navbar
